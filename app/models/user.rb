1581584e0a96f8ab40d2399198c903c6c3f9081f
Denise <hesterd1@umbc.com>
2012-04-02 22:57:25 -0400

    Added a User factory and tests for the show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require 'digest'
 class User < ActiveRecord::Base
 attr_accessor :password
 attr_accessible :name, :email, :password, :password_confirmation
@@ -12,6 +13,42 @@ validates :email, :presence => true,
 validates :password, :presence => true,
 	:confirmation => true,
 	:length => { :within => 6..40 }
+
+before_save :encrypt_password
+
+def has_password?(submitted_password)
+encrypted_password == encrypt(submitted_password)
+end
+
+class << self
+def authenticate(email, submitted_password)
+user = find_by_email(email)
+return nil if user.nil?
+return user if user.has_password?(submitted_password)
+ end
+end
+
+private
+
+def encrypt_password
+self.salt = make_salt unless has_password?(password)
+self.encrypted_password = encrypt(password)
+end
+
+def encrypt(string)
+secure_hash("#{salt}--#{string}")
+
+end
+
+def make_salt
+secure_hash("#{Time.now.utc}--#{password}")
+end  
+
+def secure_hash(string)
+Digest::SHA2.hexdigest(string) 
+
+end
+
 end
 
 # == Schema Information
