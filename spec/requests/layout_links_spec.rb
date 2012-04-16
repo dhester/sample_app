223acdb2b3e4f2a7a9d1421f0eb03e4602eefb80
Denise <hesterd1@umbc.com>
2012-04-15 20:15:29 -0400

    Integration test modification Exercise 9.6.1

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -50,11 +50,13 @@ describe "when signed in" do
 
 before(:each) do
 @user = Factory(:user)
-visit signin_path
-fill_in :email, :with => @user.email
-fill_in :password, :with => @user.password
-click_button
+integration_sign_in(@user)
+#visit signin_path
+#fill_in :email, :with => @user.email
+#fill_in :password, :with => @user.password
+#click_button
 end
+
 it "should have a signout link" do
 visit root_path
 response.should have_selector("a", :href => signout_path,
