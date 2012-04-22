f8db65f95fe678b95d64af4a5d9cbc9cd0895aba
Denise <hesterd1@umbc.com>
2012-04-21 23:21:23 -0400

    Editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_filter :authenticate, :only => [:edit, :update]
+
 def show
 @user = User.find(params[:id])
 @title = @user.name
@@ -20,7 +21,29 @@ def create
  @user.password =""
  @user.password_confirmation=""
  render 'new'
+  end
+end
 
+def edit
+@user = User.find(params[:id])
+@title = "Edit user"
+  end
+
+def update
+@user = User.find(params[:id])
+if @user.update_attributes(params[:user])
+ flash[:success] = "Profile updated."
+ redirect_to @user
+else
+@title = "Edit user"
+render 'edit'
   end
  end
-end
+
+private
+
+ def authenticate
+  deny_access unless signed_in?
+ end
+end 
+
