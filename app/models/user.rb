97d50094be0cccb02a6b0556c18f4f67af251478
Denise <hesterd1@umbc.com>
2012-05-06 20:57:28 -0400

    User relationships (following/followers)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ require 'digest'
 class User < ActiveRecord::Base
 attr_accessor :password
 attr_accessible :name, :email, :password, :password_confirmation
-has_many :microposts, :dependent => :destroy
+has_many :microposts,    :dependent => :destroy
+has_many :relationships, :foreign_key => "follower_id",
+                         :dependent => :destroy
+has_many :reverse_relationships, :foreign_key => "followed_id", 
+                                 :class_name => "Relationship", 
+                                 :dependent => :destroy 
+has_many :followers, :through => :reverse_relationships, :source => :follower 
+has_many :following, :through => :relationships, :source => :followed  
 
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :name, :presence => true,
@@ -23,6 +30,18 @@ end
 
 def feed
 Micropost.where("user_id =?", id)
+end
+
+def following?(followed)
+relationships.find_by_followed_id(followed)
+end 
+
+def follow!(followed)
+relationships.create!(:followed_id => followed.id)
+end
+
+def unfollow!(followed)
+relationships.find_by_followed_id(followed).destroy
 end
 
 class << self
